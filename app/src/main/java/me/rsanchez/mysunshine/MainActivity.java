89f53a2c90f93a21de28b0c4b806c14d1f252275
José Rubén Sánchez González <jrsg@outlook.com>
2015-07-06 23:26:49 -0500

    refresh action added

--- app/src/main/java/me/rsanchez/mysunshine/MainActivity.java
+++ app/src/main/java/me/rsanchez/mysunshine/MainActivity.java
@@ -4,17 +4,8 @@ import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 
 public class MainActivity extends ActionBarActivity {
 
@@ -24,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -49,42 +40,6 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            Uri url = Uri.parse("http://api.openweathermap.org/data/2.5/forecast?q=Veracruz,mx&mode=json&units=metric&cnt=7");
-
-            String[] weather = {
-                    "Lun - soleado - 32/29",
-                    "Mar - nublado - 31/27",
-                    "Mie - nublado - 36/32",
-                    "Jue - soleado - 36/31",
-                    "Vie - nublado - 33/29",
-                    "Sab - soleado - 31/28"
-            };
-
-            ArrayList<String> weekWeather = new ArrayList<String>(Arrays.asList(weather));
-
-            ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekWeather);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-
-            listView.setAdapter(listAdapter);
-
-            return rootView;
-        }
     }
 }
 
